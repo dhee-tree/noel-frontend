7e5c55a7aea113e3d729668a44e78a9d5757522b
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-10-20 00:30:14 +0100

    Add toast, improve barrel imports

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,4 +1,5 @@
 // This file acts as a barrel to re-export modal components for easier imports elsewhere
-export { BaseModal } from "./BaseModal/BaseModal";
-export { VerifyEmailModal } from "./VerifyEmailModal/VerifyEmailModal";
-export { JoinGroupModal } from "./JoinGroupModal/JoinGroupModal";
+export { BaseModal } from "@/components/modals/BaseModal/BaseModal";
+export { VerifyEmailModal } from "@/components/modals/VerifyEmailModal/VerifyEmailModal";
+export { JoinGroupModal } from "@/components/modals/JoinGroupModal/JoinGroupModal";
+export { CreateGroupModal } from "@/components/modals/CreateGroupModal/CreateGroupModal";
