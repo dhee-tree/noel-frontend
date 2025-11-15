07ef30ee560aed402dad2edd45c7317f970fd332
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-11-15 16:25:43 +0000

    tidy form components

--- src/components/forms/InputField.tsx
+++ src/components/forms/InputField.tsx
@@ -1,16 +1,26 @@
 "use client";
 import React from "react";
-import { Form, FloatingLabel } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { UseFormRegister, FieldError } from "react-hook-form";
 
 type InputFieldProps = {
   name: string;
   label: string;
-  type?: "text" | "email" | "password" | "number" | "date" | "datetime-local" | "time" | "tel" | "url";
+  type?:
+    | "text"
+    | "email"
+    | "password"
+    | "number"
+    | "date"
+    | "datetime-local"
+    | "time"
+    | "tel"
+    | "url";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   register: UseFormRegister<any>;
   error?: FieldError;
   placeholder?: string;
+  isRequired?: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 export const InputField = ({
@@ -19,26 +29,29 @@ export const InputField = ({
   type = "text",
   register,
   error,
-  placeholder = " ",
+  placeholder,
+  isRequired = false,
   className,
   ...props
 }: InputFieldProps) => {
   return (
     <Form.Group controlId={name} className={className}>
-      <FloatingLabel label={label}>
-        <Form.Control
-          type={type}
-          placeholder={placeholder}
-          {...register(name)}
-          isInvalid={!!error}
-          {...Object.fromEntries(
-            Object.entries(props).filter(([key]) => key !== "size")
-          )}
-        />
-        <Form.Control.Feedback type="invalid">
-          {error?.message}
-        </Form.Control.Feedback>
-      </FloatingLabel>
+      <Form.Label>
+        {label}
+        {isRequired && <span className="text-danger"> *</span>}
+      </Form.Label>
+      <Form.Control
+        type={type}
+        placeholder={placeholder}
+        {...register(name)}
+        isInvalid={!!error}
+        {...Object.fromEntries(
+          Object.entries(props).filter(([key]) => key !== "size")
+        )}
+      />
+      <Form.Control.Feedback type="invalid">
+        {error?.message}
+      </Form.Control.Feedback>
     </Form.Group>
   );
 };
